b3ce91a4f3ebe130aae40f2426b0b04103793224
caren1 <czarnocki.w@wp.pl>
2020-09-13 20:09:49 +0200

    final styling for Shortener and ShortenedLink components, next hamburger menu

--- src/components/Shortener/Shortener.jsx
+++ src/components/Shortener/Shortener.jsx
@@ -5,29 +5,29 @@ import styles from './Shortener.module.css'
 import api from '../../api/index'
 
 
-const [ input, setInput ] = useState('')
+// const [ input, setInput ] = useState('')
 // const [ links, setLinks ] = useState([])
 
 
-const onSubmit = (event) => {
-    event.preventDefault()
-    const apiResponse = api.shortenLink(input)
-    console.log(apiResponse);
-}
+// const onSubmit = (event) => {
+//     event.preventDefault()
+//     const apiResponse = api.shortenLink(input)
+//     console.log(apiResponse);
+// }
 
-const handleInputChange = (event) => {
-    event.preventDefault()
-    console.log(event.target.value);
-    setInput(event.target.value);
+// const handleInputChange = (event) => {
+//     event.preventDefault()
+//     console.log(event.target.value);
+//     setInput(event.target.value);
     
-}
+// }
 
 const Shortener = () => {
     return(
     <div className={styles.shortener}>
         <form action="">
-            <input onChange={handleInputChange} type="text" placeholder="Shorten a link here..."/>
-            <button onSubmit={onSubmit} className={styles.buttonPrimary} type="submit">Shorten It!</button>
+            <input  type="text" placeholder="Shorten a link here..."/>
+            <button className={styles.buttonPrimary} type="submit">Shorten It!</button>
         </form>
                 
     </div>
